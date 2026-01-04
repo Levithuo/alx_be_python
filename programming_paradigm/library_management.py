f098212834f00921988b9d7e6c6ad08c4f5805a2
Levithuo <levithuo75@gmail.com>
2026-01-04 03:15:49 +0300

    Add Book and Library classes for library management system

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,46 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Return True if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a Book instance to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+
+    def return_book(self, title):
+        """Return a book by title if it was checked out."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+
+    def list_available_books(self):
+        """Print all available books in the format: 'Title by Author'."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
